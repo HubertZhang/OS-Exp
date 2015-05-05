79b16cf09eebcf5b9caa40e6ff28677c7dc62adf
Ubuntu <azureuser@vmnachos.vmnachos.h9.internal.cloudapp.net>
2015-05-05 12:52:06 +0000

    change test file.

--- nachos/test/file_test.c
+++ nachos/test/file_test.c
@@ -2,7 +2,9 @@
 #include "syscall.h"
 
 int main() {
-    int fd = creat("hello");
-    printf("File: %d", fd);
+    char file_name[10] = "hello";
+    int fd = creat(file_name);
+    printf(file_name);
+    printf("File: %d\n", fd);
     return 0;
 }
